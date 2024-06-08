453debc069952f6831e26659e10a451dabc2b530
swkoyo <10441291+swkoyo@users.noreply.github.com>
2024-06-08 17:25:43 +0900

    add templ

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"binp/views"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/a-h/templ"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+func Render(ctx echo.Context, statusCode int, t templ.Component) error {
+	buf := templ.GetBuffer()
+	defer templ.ReleaseBuffer(buf)
+
+	if err := t.Render(ctx.Request().Context(), buf); err != nil {
+		return err
+	}
+
+	return ctx.HTML(statusCode, buf.String())
+}
 
 func main() {
 	if err := godotenv.Load(); err != nil {
@@ -18,7 +32,7 @@ func main() {
 	e.Use(middleware.Logger())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.JSON(200, map[string]string{"message": "Hello, World!"})
+		return Render(c, http.StatusOK, views.Base("world"))
 	})
 
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module binp
 go 1.22.4
 
 require (
+	github.com/a-h/templ v0.2.707 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/labstack/echo/v4 v4.12.0 // indirect
