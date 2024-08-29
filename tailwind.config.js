1f932ddbcc200c7f18a6245c46d9040c84e01256
swkoyo <10441291+swkoyo@users.noreply.github.com>
2024-08-29 19:06:04 +0900

    ui update init

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   darkMode: "selector",
   content: ["./views/**/*.{go,js,templ,html}"],
   theme: {
+    colors: {
+      white: "#DADDE1",
+      black: "#1E2030",
+      gray: "#2F334D",
+      blue: "#82AAFF",
+      green: "#C3E88D",
+      red: "#FF007C",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
